9adf8c0aee3da7c26ac5bb214310da61b4dec949
Ian Frankenburg <31015994+ian-frankenburg@users.noreply.github.com>
2020-07-05 14:39:55 -0400

    variable importance tinkering

--- R/tryingBART-Stack.R
+++ R/tryingBART-Stack.R
@@ -13,7 +13,7 @@ p<- 5000
 # epsilon <- rnorm(N)
 # xcov <- matrix(runif(N*p), nrow=N)
 # y <- sin(pi*xcov[,1]*xcov[,2]) + 20*(xcov[,3]-0.5)^2+10*xcov[,4]+5*xcov[,5]+epsilon
-# 
+#
 # epsilontest <- rnorm(N)
 # xcovtest <- matrix(runif(N*p), nrow=N)
 # ytest <- sin(pi*xcovtest[,1]*xcovtest[,2]) + 20*(xcovtest[,3]-0.5)^2+10*xcovtest[,4]+5*xcovtest[,5]+epsilontest
@@ -21,35 +21,35 @@ p<- 5000
 # #####################
 # DATA PROCESSING
 #####################
-# library(mlbench)
-# data(BostonHousing)
-# y <- BostonHousing$medv
-# X <- BostonHousing[,-which(colnames(BostonHousing)=="medv")]
-# X$chas <- as.numeric(X$chas)
-# dat <- cbind(y,X)
+library(mlbench)
+data(BostonHousing)
+y <- BostonHousing$medv
+X <- BostonHousing[,-which(colnames(BostonHousing)=="medv")]
+X$chas <- as.numeric(X$chas)
+dat <- cbind(y,X)
 
 # #####################
 # DATA PROCESSING
 #####################
 # auto data
-library(ISLR)
-data("Auto")
-dat <- Auto[complete.cases(Auto),]
-dat <- dat[,-ncol(dat)]
-colnames(dat)[1] <- "y"
+# library(ISLR)
+# data("Auto")
+# dat <- Auto[complete.cases(Auto),]
+# dat <- dat[,-ncol(dat)]
+# colnames(dat)[1] <- "y"
 
 start.time <- Sys.time()
 ### INCLUDE SPURIOUS
-M <- 1000
+M <- 500
 colnames(dat)[1] <- "y"
 spurious <- matrix(runif(nrow(dat)*M,0,1), nrow(dat), M)
 colnames(spurious) <- paste0("X", 1:M)
 dat <- cbind(dat, spurious)
 
-
+q = 1
 runs <- 2
 mse1=mse2=mse3=mse4=sizes=rep(0,runs)
-stackwts=bmawts=list()
+stackwts=bmawts=bart_bma=list()
 for(q in 1:runs){
 train_ind = sample(seq_len(nrow(dat)),size = floor(.8*nrow(dat)), replace=F)
 train = dat[train_ind,]
@@ -57,8 +57,8 @@ test = dat[-train_ind,]
 ytest = as.vector(test$y)
 xcov <- as.matrix(train[,-1]); y <- as.vector(train$y)
 xcovtest <- as.matrix(test[,-1])
-bart_bma <- bartBMA(x.train = xcov, y.train=y, x.test=xcovtest)
-while(length(bart_bma$sumoftrees)==1){
+bart_bma[[q]] <- bartBMA.default(x.train = xcov, y.train=y, x.test=xcovtest)
+while(length(bart_bma[[q]]$sumoftrees)==1){
   print("1 model error")
   train_ind = sample(seq_len(nrow(dat)),size = floor(.8*nrow(dat)), replace=F)
   train = dat[train_ind,]
@@ -66,9 +66,9 @@ while(length(bart_bma$sumoftrees)==1){
   ytest = as.vector(test$y)
   xcov <- as.matrix(train[,-1]); y <- as.vector(train$y)
   xcovtest <- as.matrix(test[,-1])
-  bart_bma <- bartBMA(x.train = xcov, y.train=y, x.test=xcovtest)
+  bart_bma[[q]] <- bartBMA(x.train = xcov, y.train=y, x.test=xcovtest)
 }
-foo <- pred_intervals(bart_bma, num_iter = 5000, burnin = 1000, l_quant = .25, u_quant = .75)
+foo <- pred_intervals(bart_bma[[q]], num_iter = 5000, burnin = 1000, l_quant = .25, u_quant = .75)
 #  get posterior draws of sigma and beta
 sigma_chains <- foo[[2]]
 beta_chains <- foo[[3]]
@@ -133,38 +133,7 @@ parseTreeSums <- function(bart_bma){
     )
   )
 }
-parsed <- parseTreeSums(bart_bma)
-
-#### FOR TESTING POINTWISE LOG-LIKELIHOOD BATCH COMPUTING CODE
-# mean <- rep(0,length(y))
-# i=j=1
-# mean_list <- list()
-# # trying to just construct mean function by defintion
-# meanbuilder <- function(anovas, beta, mcmcdraw){
-#   for(i in 1:length(anovas)){
-#     sum_trees <- parsed$anovas[[i]]
-#     params <- beta_chains[[i]]
-#     for(j in 1:length(sum_trees)){
-#       temp <- as.matrix(sum_trees[[j]])
-#       mean = mean + temp %*% params[mcmcdraw, 1:ncol(temp)]
-#       params <- params[,-c(1:ncol(temp))]
-#       j=j+1
-#     }
-#     mean_list[[i]] = mean
-#   }
-#   return(mean_list)
-# }
-# a <- rep(0,4000)
-# for(i in 1000:5000){
-# mcmcdraw=i
-# t = 1
-# obs = 2
-# mu <- meanbuilder(parsed$anovas, beta_chains, mcmcdraw)
-# sigma <- sigma_chains[[t]][mcmcdraw]
-# a[i] <- dnorm(y[obs], mu[[t]][obs], sd = sigma, log=T)
-# }
-# plot(density(a))
-# View(log_like_mat[[t]])
+parsed <- parseTreeSums(bart_bma[[q]])
 
 ### USE POSTERIOR DRAWS TO EVALUATE POINT-WISE LIKELIHOOD NEEDED FOR STACKING
 temp <- rep(0, length(sigma_chains))
@@ -189,19 +158,19 @@ for(i in 1:length(sigma_chains)){
 }
 wts = loo_model_weights(loolist, method = "stacking")
 #pseud = loo_model_weights(loolist, method = "pseudobma")
-b <- exp(bart_bma$bic)/sum(exp(bart_bma$bic))
+b <- exp(bart_bma[[q]]$bic)/sum(exp(bart_bma[[q]]$bic))
 
 stackwts[[q]] = wts
 bmawts[[q]] = b
 
 sizes[q] <- length(loolist)
-bmapred <- predict.bartBMA(bart_bma, newdata = xcovtest)
-stackpred <- predict.bartStack(bart_bma, newdata = xcovtest)
+bmapred <- predict.bartBMA(bart_bma[[q]], newdata = xcovtest)
+stackpred <- predict.bartStack(bart_bma[[q]], newdata = xcovtest)
 #pseudpred <- predict.bartPseud(bart_bma, newdata = xcovtest)
 mse1[q] <- mse(ytest, bmapred)
 mse2[q] <- mse(ytest, stackpred)
 #mse3[q] <- mse(ytest, pseudpred)
-rf <- predict(randomForest(x = xcov, y = y, ntree = length(bart_bma$sumoftrees)), newdata = xcovtest)
+rf <- predict(randomForest(x = xcov, y = y, ntree = length(bart_bma[[q]]$sumoftrees)), newdata = xcovtest)
 mse4[q] <- mse(ytest, rf)
 print(q)
 }
@@ -213,4 +182,28 @@ as.data.frame(cbind(
   "Stacking"=mse2,
   "RF"=mse4)
 )
+w=2
+for(w in 1:length(stackwts)){
+  sumtemp = bart_bma[[w]]$sumoftrees
+  sumotree = sumtemp[which(as.numeric(stackwts[[w]]) > .1)]
+  a = varIncProb(bart_bma[[w]],score = stackwts[[w]],numvars = bart_bma[[w]]$numvars,sumoftrees=sumotree)
+  print(a[which(a>0)])
+}
+w=2
+for(w in 1:length(bmawts)){
+  sumtemp = bart_bma[[w]]$sumoftrees
+  sumotree = sumtemp[which(as.numeric(bmawts[[w]]) > .1)]
+  a=varIncProb(bart_bma[[w]],score = bmawts[[w]],numvars = bart_bma[[w]]$numvars,sumoftrees=sumotree)
+  print(a[which(a>0)])
+}
+
+
+which(varImpScores(bart_bma[[1]], scores = stackwts[[1]])!=0)
+which(varImpScores(bart_bma[[1]], scores = bmawts[[1]])!=0)
+
+which(varImpScores(bart_bma[[2]], scores = stackwts[[2]])!=0)
+which(varImpScores(bart_bma[[2]], scores = bmawts[[2]])!=0)
+
+varIncProb(bart_bma[[2]], score = stackwts[[2]])
+varIncProb(bart_bma[[2]], score = bmawts[[2]])
 
--- R/varImpScores.R
+++ R/varImpScores.R
@@ -1,6 +1,7 @@
-varImpScores<-function(object,...){
+varImpScores<-function(object,scores,...){
   #object will be bartBMA object.
-  imp_vars2=get_weighted_var_imp(num_vars=object$numvars,BIC=object$bic,sum_trees=object$sumoftrees)
+  ### my code: changed BIC=object$bic to scores arg. in function
+  imp_vars2=get_weighted_var_imp(num_vars=object$numvars,BIC=scores,sum_trees=object$sumoftrees)
   res<-apply(imp_vars2[[4]],2,sum)
   class(res)<-"varImpScores.bartBMA"  
   res
